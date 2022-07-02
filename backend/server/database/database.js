b78506a1c1e3c9b89113ea2ab65113755c7c30f1
Danijel Drazetic <daddo.1009@gmail.com>
2022-07-02 16:46:00 +0200

    backend info

--- backend/server/database/database.js
+++ backend/server/database/database.js
@@ -1,12 +1,17 @@
-const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv');
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
-dotenv.config({path: 'config.env'});
+dotenv.config({ path: "config.env" });
 const dbname = process.env.db_name;
 const user = process.env.db_user;
 const pass = process.env.db_pass;
 
-module.exports = new Sequelize(dbname, user, pass, {
-    host: 'localhost',
-    dialect: 'postgres'
+// module.exports = new Sequelize(dbname, user, pass, {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
+
+module.exports = new Sequelize("postgres://postgres:postgres@localhost/dada", {
+  dialect: "postgres",
+  // anything else you want to pass
 });
